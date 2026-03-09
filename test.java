b4b3de62cc467101aa131f440bd9c36318cc7f88
DevaZimble <devadath.ks@zimblesystems.com>
2026-03-09 09:34:41 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,9 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
